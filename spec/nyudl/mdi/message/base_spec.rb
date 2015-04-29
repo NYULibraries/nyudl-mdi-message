e99f9738d76e82a9ac24ebcc251e38d1d4d4758e
jgpawletko <jgpawletko@gmail.com>
2015-04-29 11:27:13 -0400

    add initial RSpec example

--- spec/nyudl/mdi/message/base_spec.rb
+++ spec/nyudl/mdi/message/base_spec.rb
@@ -2,5 +2,10 @@ require 'spec_helper'
 
 module NYUDL::MDI::Message
   describe Base do
+    context 'when instantiated' do
+      it 'should be the correct class' do
+        expect(NYUDL::MDI::Message::Base.new).to be_an_instance_of(NYUDL::MDI::Message::Base)
+      end
+    end
   end
 end
