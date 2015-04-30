8a759ae4aac32b2e4e1e4bcb4bf95bc0bb22af34
jgpawletko <jgpawletko@gmail.com>
2015-04-30 16:46:25 -0400

    add remaining Response#start_time= examples, implementation

--- lib/nyudl/mdi/message/response.rb
+++ lib/nyudl/mdi/message/response.rb
@@ -1,9 +1,16 @@
+require 'timeliness'
+
 module NYUDL::MDI::Message
   class Response < Base
     def start_time
       h[:start_time]
     end
     def start_time=(value)
+
+      unless(Timeliness.parse(value, format: 'yyyy-mm-ddThh:nn:ssZ'))
+        raise ArgumentError, "non-UTC ISO-8601 time detected: #{value}"
+      end
+
       h[:start_time] = value
     end
   end
--- spec/nyudl/mdi/message/response_spec.rb
+++ spec/nyudl/mdi/message/response_spec.rb
@@ -22,8 +22,17 @@ module NYUDL::MDI::Message
           expect(response.start_time).to be == "#{time}"
         end
       end
-      pending "when assigned an invalid time"
-      pending "when assigned a non-UTC time"
+      context "when assigned an invalid time" do
+        it "raises an ArgumentError" do
+          expect { response.start_time = 'foo' }.to raise_error(ArgumentError)
+        end
+      end
+      context "when assigned a non-UTC time" do
+        it "raises an ArgumentError" do
+          time = Time.now.asctime
+          expect { response.start_time = time }.to raise_error(ArgumentError)
+        end
+      end
     end
   end
 end
