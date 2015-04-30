c80d7a39b4e7dd075868927e50f2498eb235c8c1
jgpawletko <jgpawletko@gmail.com>
2015-04-30 16:50:52 -0400

    refactor response_spec.rb

--- spec/nyudl/mdi/message/response_spec.rb
+++ spec/nyudl/mdi/message/response_spec.rb
@@ -3,18 +3,22 @@ require 'time'
 
 module NYUDL::MDI::Message
   describe Response do
+    let(:response) { NYUDL::MDI::Message::Response.new }
+
     context 'when instantiated' do
       it 'should be the correct class' do
-        expect(NYUDL::MDI::Message::Response.new).to be_an_instance_of(NYUDL::MDI::Message::Response)
+        expect(response).to be_an_instance_of(NYUDL::MDI::Message::Response)
       end
     end
+
+
     describe "#start_time" do
-      let(:response) { NYUDL::MDI::Message::Response.new }
       context "when not assigned a value" do
         it "returns nil" do
           expect(response.start_time).to be_nil
         end
       end
+
       context "when assigned a valid time" do
         it "returns the assigned time" do
           time = Time.now.utc.iso8601
@@ -22,11 +26,16 @@ module NYUDL::MDI::Message
           expect(response.start_time).to be == "#{time}"
         end
       end
+    end
+
+
+    describe "#start_time=" do
       context "when assigned an invalid time" do
         it "raises an ArgumentError" do
           expect { response.start_time = 'foo' }.to raise_error(ArgumentError)
         end
       end
+
       context "when assigned a non-UTC time" do
         it "raises an ArgumentError" do
           time = Time.now.asctime
@@ -34,5 +43,7 @@ module NYUDL::MDI::Message
         end
       end
     end
+
+
   end
 end
