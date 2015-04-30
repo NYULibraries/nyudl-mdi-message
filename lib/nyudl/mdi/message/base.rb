d146ef09fe0d0df5996999613ddb50f0098a2231
jgpawletko <jgpawletko@gmail.com>
2015-04-29 21:36:22 -0400

    add hash to Base class

--- lib/nyudl/mdi/message/base.rb
+++ lib/nyudl/mdi/message/base.rb
@@ -2,14 +2,21 @@ require 'securerandom'
 
 module NYUDL::MDI::Message
   class Base
+    def initialize
+      h[:version] = MESSAGE_STRUCTURE_VERSION
+    end
     def version
-      MESSAGE_STRUCTURE_VERSION
+      h[:version]
     end
     def to_json
       "{}"
     end
     def request_id
       @request_id ||= SecureRandom.uuid
+    end
+    private
+    def h
+      @h ||= {}
     end
   end
 end
