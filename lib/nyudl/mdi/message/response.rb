01dd1c67b401a592b9b3f0b8adc103a57d494fc8
jgpawletko <jgpawletko@gmail.com>
2015-04-30 16:59:56 -0400

    add Response#end_time examples and implementation

--- lib/nyudl/mdi/message/response.rb
+++ lib/nyudl/mdi/message/response.rb
@@ -8,12 +8,25 @@ module NYUDL::MDI::Message
     end
 
     def start_time=(value)
-
       unless(Timeliness.parse(value, format: 'yyyy-mm-ddThh:nn:ssZ'))
         raise ArgumentError, "non-UTC ISO-8601 time detected: #{value}"
       end
 
       h[:start_time] = value
+    end
+
+
+    def end_time
+      h[:end_time]
+    end
+
+    def end_time=(value)
+
+      unless(Timeliness.parse(value, format: 'yyyy-mm-ddThh:nn:ssZ'))
+        raise ArgumentError, "non-UTC ISO-8601 time detected: #{value}"
+      end
+
+      h[:end_time] = value
     end
   end
 end
--- spec/nyudl/mdi/message/response_spec.rb
+++ spec/nyudl/mdi/message/response_spec.rb
@@ -45,5 +45,39 @@ module NYUDL::MDI::Message
     end
 
 
+
+    describe "#end_time" do
+      context "when not assigned a value" do
+        it "returns nil" do
+          expect(response.end_time).to be_nil
+        end
+      end
+
+      context "when assigned a valid time" do
+        it "returns the assigned time" do
+          time = Time.now.utc.iso8601
+          response.end_time = time
+          expect(response.end_time).to be == "#{time}"
+        end
+      end
+    end
+
+
+    describe "#end_time=" do
+      context "when assigned an invalid time" do
+        it "raises an ArgumentError" do
+          expect { response.end_time = 'foo' }.to raise_error(ArgumentError)
+        end
+      end
+
+      context "when assigned a non-UTC time" do
+        it "raises an ArgumentError" do
+          time = Time.now.asctime
+          expect { response.end_time = time }.to raise_error(ArgumentError)
+        end
+      end
+    end
+
+
   end
 end
