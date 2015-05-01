a3cfc5b1b910e199e2b8412a5fadba17654066e5
jgpawletko <jgpawletko@gmail.com>
2015-05-01 07:43:29 -0400

    add Response#agent functionality

--- lib/nyudl/mdi/message/response.rb
+++ lib/nyudl/mdi/message/response.rb
@@ -33,20 +33,44 @@ module NYUDL::MDI::Message
     end
 
 
+    def agent
+      h[:agent]
+    end
+
+    def agent=(value)
+      assert_valid_agent!(value)
+      h[:agent] = value
+    end
+
+
     private
 
-    def assert_iso8601_utc!(value)
-      unless Timeliness.parse(value, format: 'yyyy-mm-ddThh:nn:ssZ')
-        raise ArgumentError, "non-UTC ISO-8601 time detected: #{value}"
+    def assert_iso8601_utc!(time)
+      unless Timeliness.parse(time, format: 'yyyy-mm-ddThh:nn:ssZ')
+        raise ArgumentError, "non-UTC ISO-8601 time detected: #{time}"
       end
     end
 
-    def assert_valid_outcome!(value)
-      valid_values = %w(success error)
-      unless valid_values.include?(value)
-        raise ArgumentError, "invalid outcome detected: #{value}"
+    def assert_valid_outcome!(outcome)
+      valid_outcomes = %w(success error)
+      unless valid_outcomes.include?(outcome)
+        raise ArgumentError, "invalid outcome detected: #{outcome}"
       end
     end
 
+    def assert_valid_agent!(agent)
+
+      raise ArgumentError, "agent must be a Hash" unless agent.is_a?(Hash)
+
+      required_keys = [ :name, :version , :host ].sort
+      unless agent.keys.sort == required_keys
+        raise ArgumentError, "invalid keys: #{agent.keys}"
+      end
+
+      values = (agent.values.reject {|a| a.strip.empty? })
+      unless values.length == required_keys.length
+        raise ArgumentError, "all keys must have values: #{agent}"
+      end
+    end
   end
 end
