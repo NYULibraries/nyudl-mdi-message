59eb32b2227328c6fd01f220f0b38bb250ee4230
jgpawletko <jgpawletko@gmail.com>
2015-05-04 22:42:10 -0400

    uprev gem version to 0.2.0

--- lib/nyudl/mdi/message/version.rb
+++ lib/nyudl/mdi/message/version.rb
@@ -1,7 +1,7 @@
 module NYUDL
   module MDI
     module Message
-      VERSION = '0.1.0'         # gem version
+      VERSION = '0.2.0'         # gem version
       MESSAGE_STRUCTURE_VERSION = '0.1.0'
     end
   end
