25517eb57d2293b63452092b9e1d039a0d90b41e
jgpawletko <jgpawletko@gmail.com>
2015-05-05 09:13:32 -0400

    clean up version.rb

--- lib/nyudl/mdi/message/version.rb
+++ lib/nyudl/mdi/message/version.rb
@@ -1,5 +1,6 @@
 module NYUDL
   module MDI
+    # Module contains classes for messages that conform to a given format
     module Message
       VERSION = '0.2.0'         # gem version
       MESSAGE_STRUCTURE_VERSION = '0.1.0'
