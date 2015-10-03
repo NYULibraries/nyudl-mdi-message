e09d2ec55b93c821c680fe937932067925422361
jgpawletko <jgpawletko@gmail.com>
2015-10-02 22:12:23 -0400

    preserve incoming message version, gem to 0.3.0

--- lib/nyudl/mdi/message/version.rb
+++ lib/nyudl/mdi/message/version.rb
@@ -2,7 +2,7 @@ module NYUDL
   module MDI
     # Module contains classes for messages that conform to a given format
     module Message
-      VERSION = '0.2.1'         # gem version
+      VERSION = '0.3.0'         # gem version
       MESSAGE_STRUCTURE_VERSION = '0.1.0'
     end
   end
